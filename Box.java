ce55136fef9cb9eb82e0307f6506ecaa28b131fd
Jujua_c <juliann.merit@gmail.com>
2023-03-17 16:50:08 +0100

    Update Box.java

--- Box.java
+++ Box.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
 public class Box {
     public Box() {
     System.out.println("Box créee");
     }
+
+    ArrayList<String> contents = new ArrayList<String>();
+    public void add(String truc) {
+        this.contents.add(truc);
+    }
+
 }
