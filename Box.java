25e77eef9cb7b3f3244ff2687dc97510d7c4774f
Jujua_c <43277613+Jujua31@users.noreply.github.com>
2023-03-24 15:31:00 +0100

    Change box + exception

--- Box.java
+++ Box.java
@@ -1,12 +1,24 @@
 import java.util.ArrayList;
 public class Box {
     public Box() {
-    System.out.println("Box créee");
+        System.out.println("Box créee");
     }
 
-    ArrayList<String> contents = new ArrayList<String>();
+    private ArrayList<String> contents = new ArrayList<String>();
+    
     public void add(String truc) {
         this.contents.add(truc);
     }
 
+    public boolean contient(String truc){
+        return this.contient(truc);
+    }
+
+    public void remove(String truc) throws{
+        if(!this.contient(truc)){
+            throw new RemoveException();
+        }else{
+            this.contents.remove(truc);
+        }
+    }
 }
---
+++ RemoveException.java
@@ -0,0 +1,5 @@
+import java.lang.reflect.Executable;
+
+public class RemoveException extends Exception{
+    System.out.println("N'existe pas dans Box donc ne peux pas être retier");
+}
